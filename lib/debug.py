0ba83fd781893782cff9c7f32c0e32fdaaca1f97
jacklyne <jacklyne.owuor@student.moringaschool>
2025-11-27 22:51:34 +0300

    added content

--- lib/debug.py
+++ lib/debug.py
@@ -1,18 +1,27 @@
-
 #!/usr/bin/env python3
-# lib/testing/debug.py
+#lib/testing/debug.py
 
 from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
 
-# Call the correct method name and pass no arguments
-Department.create_table()
+Department.drop_table()
 Department.create_table()
 
-management =Department("Management","Kisumu")
-agriculture =Department.create("Agriculture,Nandi")
-hr = Department.creareate("HR","Nairobi")
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)  # <Department 2: HR, Building F, 10th Floor>
+
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
 
 ipdb.set_trace()
